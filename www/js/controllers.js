4e5c8d1d5f31df64f820c743bb5a370c5adf845d
segito <segito10@gmail.com>
2016-05-24 01:22:15 +0000

    FIRST MOBILE APP SKELETON

---
+++ www/js/controllers.js
@@ -0,0 +1,30 @@
+angular.module('app.controllers', [])
+  
+.controller('accueilCtrl', function($scope) {
+
+})
+   
+.controller('constatIdentitCtrl', function($scope) {
+
+})
+   
+.controller('pVCtrl', function($scope) {
+
+})
+   
+.controller('recapitulatifCtrl', function($scope) {
+
+})
+   
+.controller('envoieCtrl', function($scope) {
+
+})
+   
+.controller('mesConstatsCtrl', function($scope) {
+
+})
+   
+.controller('afficherConstatCtrl', function($scope) {
+
+})
+ 
